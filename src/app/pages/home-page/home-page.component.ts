bb74ae89d0db64d173ecc84c7c7f1cf24679adfc
Tales <tales.a.s.furlan@gmail.com>
2024-02-25 12:40:15 -0300

    feat(home-page): implement home-page base

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {
+export class HomePageComponent implements AfterViewInit {
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
+  ngAfterViewInit() {
+    const columns = this.el.nativeElement.querySelectorAll('.column');
+
+    columns.forEach((column: HTMLElement) => {
+      this.renderer.listen(column, 'scroll', () => {
+        this.renderer.addClass(column, 'scrolling');
+      });
+
+      let timer: any = null;
+      this.renderer.listen(column, 'scroll', () => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+          this.renderer.removeClass(column, 'scrolling');
+        }, 150);
+      });
+    });
+  }
 }
