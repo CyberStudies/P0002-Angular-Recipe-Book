8ca91a0b6f114a3ba167b0771a062f1826117391
Tales <tales.a.s.furlan@gmail.com>
2024-02-26 19:14:57 -0300

    feat(home-page): Fix span

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -9,21 +9,6 @@ export class HomePageComponent implements AfterViewInit {
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   ngAfterViewInit() {
-    const columns = this.el.nativeElement.querySelectorAll('.column');
-
-    columns.forEach((column: HTMLElement) => {
-      this.renderer.listen(column, 'scroll', () => {
-        this.renderer.addClass(column, 'scrolling');
-      });
-
-      let timer: any = null;
-      this.renderer.listen(column, 'scroll', () => {
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-          this.renderer.removeClass(column, 'scrolling');
-        }, 150);
-      });
-    });
     // Sort the recipes array by likes in descending order
     this.recipes.sort((a, b) => b.likes - a.likes);
 
