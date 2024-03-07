4f37b35b8969b4315bd975d96df8c837dcc9a919
Tales Augusto <tales.a.s.furlan@gmail.com>
2024-03-07 10:01:54 -0300

    feat(selected-recipe): implement recipe page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,38 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import recipes from '../../utils/recipes';
 import { Router } from '@angular/router';
 
-@Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss'],
-})
-export class HomePageComponent implements OnInit {
-  sortedRecipes: Recipe[] = [];
-  constructor(private router: Router) {}
-
-  ngOnInit() {
-    // Create a copy of the recipes array
-    this.sortedRecipes = [...this.allRecipes];
-
-    // Sort the copied array by likes in descending order
-    this.sortedRecipes.sort((a, b) => b.likes - a.likes);
-
-    // Get the top 5 recipes
-    this.sortedRecipes = this.sortedRecipes.slice(0, 5);
-  }
-
-  allRecipes: Recipe[] = recipes;
-  goTo(id: Number) {
-    this.router.navigate(['/selected-recipe', id]);
-  }
-}
-
 interface Recipe {
-  id: Number;
+  id: number;
   name: string;
   ingredients: string[];
   image: string;
   date: number;
   likes: number;
+}
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HomePageComponent implements OnInit {
+  sortedRecipes: Recipe[] = [];
+  allRecipes: Recipe[] = recipes.map((recipe) => ({
+    ...recipe,
+    id: Number(recipe.id),
+  }));
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.sortedRecipes = [...this.allRecipes];
+    this.sortedRecipes.sort((a, b) => b.likes - a.likes);
+    this.sortedRecipes = this.sortedRecipes.slice(0, 5);
+  }
+
+  goto(id: number) {
+    this.router.navigate(['/selected-recipe', id]);
+  }
+
+  trackByRecipeId(index: number, recipe: Recipe): number {
+    return recipe.id;
+  }
 }
--- src/app/pages/selected-recipe/selected-recipe.component.ts
+++ src/app/pages/selected-recipe/selected-recipe.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-selected-recipe',
   templateUrl: './selected-recipe.component.html',
-  styleUrl: './selected-recipe.component.scss'
+  styleUrl: './selected-recipe.component.scss',
 })
-export class SelectedRecipeComponent {
-
-}
+export class SelectedRecipeComponent {}
