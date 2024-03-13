7b9d1d4a7042a8ee6473a02b706f30522cd1284d
Tales Augusto <tales.a.s.furlan@gmail.com>
2024-03-13 19:28:25 -0300

    feat(themes): implement cleber themes

--- src/app/pages/selected-recipe/selected-recipe.component.ts
+++ src/app/pages/selected-recipe/selected-recipe.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-selected-recipe',
   templateUrl: './selected-recipe.component.html',
   styleUrl: './selected-recipe.component.scss',
 })
-export class SelectedRecipeComponent {}
+export class SelectedRecipeComponent {
+  constructor(private route: ActivatedRoute) {
+    this.route.params.subscribe((params) => {
+      const productId = params['id'];
+    });
+  }
+}
